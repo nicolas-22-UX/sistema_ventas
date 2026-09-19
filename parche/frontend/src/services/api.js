1ce0fed887be159ce4bc8245763858973de84206
nicolas-22-UX <nicoapariciocamacho@gmail.com>
2026-09-19 10:44:49 -0500

    Delete parche directory

--- parche/frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-//Instancia de axios que apunte al backend
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  timeout: 5000
-});
-
-export default api;
